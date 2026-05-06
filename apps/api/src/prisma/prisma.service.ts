09b2beed5c708181aa8c1d2e3ff3673aa7b31be2
mvinduwara <mvinduwara@gmail.com>
2026-05-06 14:35:06 +0530

    new commit

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -4,7 +4,6 @@ import { PrismaClient } from 'db';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
   }
 }
