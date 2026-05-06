2c422491e1b96fc864af431361ccf55ed273ffcf
mvinduwara <mvinduwara@gmail.com>
2026-05-06 14:32:14 +0530

    new commit

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from 'db'; // Importing from your custom monorepo package!
+import { PrismaClient } from 'db';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
   }
 }
