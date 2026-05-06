d8622acc7628ca441dd26fb811ae933b6b725802
mvinduwara <mvinduwara@gmail.com>
2026-05-06 16:03:14 +0530

    new ccommit

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,47 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import {
+  KeycloakConnectModule,
+  ResourceGuard,
+  RoleGuard,
+  AuthGuard,
+} from 'nest-keycloak-connect';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { InstitutionsModule } from './institutions/institutions.module';
 import { UsersModule } from './users/users.module';
+import { InstitutionsModule } from './institutions/institutions.module';
 
 @Module({
-  imports: [PrismaModule, InstitutionsModule, UsersModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    InstitutionsModule,
+    // Initialize Keycloak
+    KeycloakConnectModule.register({
+      authServerUrl:
+        process.env.KEYCLOAK_AUTH_SERVER_URL || 'http://localhost:8080',
+      realm: process.env.KEYCLOAK_REALM || 'lms-realm',
+      clientId: process.env.KEYCLOAK_CLIENT_ID || 'api-client',
+      secret: process.env.KEYCLOAK_SECRET || 'secret-placeholder',
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class AppModule {}
