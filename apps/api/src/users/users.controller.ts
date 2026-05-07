2e244358c3d429fdfc80c57b537056bb0ddc3190
mvinduwara <mvinduwara@gmail.com>
2026-05-07 14:40:23 +0530

    Update users.controller.ts

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Roles } from 'nest-keycloak-connect';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @Roles({ roles: ['ADMIN'] })
+  async create(@Body() body: { email: string; role: any; institutionId: string }) {
+    return this.usersService.createUser(body);
+  }
+
+  @Get()
+  @Roles({ roles: ['ADMIN'] })
+  async findAll() {
+    return this.usersService.getAllUsers();
+  }
+}
